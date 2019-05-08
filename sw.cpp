0817cfaa13bd5bf26c014285aa749c92df87b9b7
lzwdgc <lzwdgc@gmail.com>
2019-05-09 00:03:15 +0300

    Add sw build. Fix real days in lamp quest.

---
+++ sw.cpp
@@ -0,0 +1,15 @@
+void build(Solution &s)
+{
+    auto &bot = s.addTarget<ExecutableTarget>("mechanoids_quest");
+    bot.CPPVersion = CPPLanguageStandard::CPP17;
+    bot.PackageDefinitions = true;
+    bot += "mechanoids_quest.cpp";
+    bot +=
+        "pub.egorpugin.primitives.filesystem-master"_dep,
+        "pub.egorpugin.primitives.templates-master"_dep,
+        "pub.egorpugin.primitives.sw.main-master"_dep,
+        "org.sw.demo.lua-5"_dep,
+        "org.sw.demo.fmt-5"_dep,
+        "org.sw.demo.reo7sp.tgbot-master"_dep
+    ;
+}
